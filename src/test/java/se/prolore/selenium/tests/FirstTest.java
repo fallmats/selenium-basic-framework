f01cd003a042776677bc5248e8d3907862e0ed7b
mats <fallmats@gmail.com>
2019-03-26 19:41:17 +0100

    Added a simple test class to use for a first exercise

---
+++ src/test/java/se/prolore/selenium/tests/FirstTest.java
@@ -0,0 +1,52 @@
+package se.prolore.selenium.tests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class FirstTest {
+
+    private String osName = System.getProperty("os.name").toUpperCase();
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        if (isMac()) System.setProperty("webdriver.chrome.driver", "./lib/chromedriver-osx");
+        if (isWindows()) System.setProperty("webdriver.chrome.driver", "./lib/chromedriver-win.exe");
+        if (isNix()) System.setProperty("webdriver.chrome.driver", "./lib/chromedriver-nix");
+
+
+        DesiredCapabilities capa = new DesiredCapabilities();
+        capa.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        capa.setCapability("nativeEvents", true);
+
+        driver = new ChromeDriver(capa);
+    }
+
+    @Test
+    public void aFirstTest() {
+        driver.get("https://www.phptravels.net/");
+    }
+
+
+    @After
+    public void cleanup() {
+        driver.quit();
+    }
+
+
+    private boolean isMac() { return osName.contains("MAC"); }
+    private boolean isWindows() {
+        return osName.contains("WIN");
+    }
+    private boolean isNix() {
+        return osName.contains("NIX") || osName.contains("NUX") || osName.contains("AIX");
+    }
+
+}
